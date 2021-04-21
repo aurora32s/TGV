bf108a3d329161f944cf22c2726380fe2f57dcfc
yeonju.jin <aurora32s@naver.com>
2021-04-21 17:34:54 +0900

    DB 설계서 & REST API 설계서 추가

--- tsis_movie_client/src/main.js
+++ tsis_movie_client/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// css
+import './assets/css/css.css'
+import './assets/css/layout.css'
+
+// utils
+import util from './utils/commonUtil.js'
+Vue.mixin(util)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
